e332d4b5ace4396d01b2affad39d9f47e64eb422
Bogdan Tudorache <bogdantudorache@Bogdans-MacBook-Air.local>
2024-06-21 19:52:45 +0300

    Second commit

--- Nexus.java
+++ Nexus.java
@@ -1,7 +1,18 @@
 
 public static void main (String[] args){
     System.out.println("Hello world");
+    findMax();
 }
 
 
+public  static void   findMax(){
+    int max =0;
 
+    int[] maxArray = {1,2,3,4,5};
+    for (int i=0; i<4;i++){
+        if (max<maxArray[i]){
+            max = maxArray[i];
+        }
+    }
+    System.out.println("Numarul maxim este: " + max);
+}
